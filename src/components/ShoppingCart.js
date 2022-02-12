cd1d85d00e2dffb3e7600f3693ce4646e7f7511c
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-02-12 14:11:16 -0800

    added empty cart View depending on the length of item inside the cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -14,6 +14,11 @@ import ShoppingCartItem from './ShoppingCartItem';
 const ShoppingCart = props => {
   return (
     <ScrollView>
+      {props.cartContents.length == 0 ? (
+        <View style={styles.cartEmpty}><Text style={styles.cartEmptyText} >Cart is empty! Add Product to view</Text></View>
+      ) : (
+        <></>
+      )}
       {props.cartContents.map(content => (
         <ShoppingCartItem key={content.id} item={content} />
       ))}
@@ -24,6 +29,20 @@ const ShoppingCart = props => {
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  cartEmpty: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    backgroundColor: 'orange',
+    height: 100,
+  },
+  cartEmptyText:{
+    fontSize: 20,
+  },
+});
 
 const mapStateToProps = state => {
   return {
